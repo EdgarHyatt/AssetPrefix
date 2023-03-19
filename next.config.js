af38e0886782529410cf48234b44abd4f2347720
Admin <admin@CHICO-CC02GQ1PD.local>
2023-03-19 15:48:42 -0600

    AssetsPrefix with basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: 'https://cdn.jsdelivr.net/gh/EdgarRepos/cdn/',
+  basePath: '/basePath',
 }
 
 module.exports = nextConfig
